35b71727678270556f85711524e06a933f4bbf73
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-08-13 16:08:01 +0100

    finished main app setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,30 @@
-console.log("Hello World!")
+//Load environment variable
+import { config } from "dotenv";
+config();
+
+// Import express, cors, and morgan
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+// Set up express
+const app = express();
+app.disable("x-powered-by");
+app.use(cors());
+app.use(morgan("dev"));
+// Tell express to use a JSON parser middleware
+app.use(express.json());
+// Tell express to use a URL Encoding middleware
+app.use(express.urlencoded({ extended: true }));
+
+//Add routers below:
+
+app.get("/*splat", async (req, res) => {
+  res.send("ok");
+});
+
+// Start API server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`\n Server is running on http://localhost:${PORT}\n`);
+});
