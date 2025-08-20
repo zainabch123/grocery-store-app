6ccb60bfd814b8095af5a03bf0c9916b91b013b5
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-08-20 18:33:17 +0100

    added login page

--- frontend/src/pages/login/loginPage.jsx
+++ frontend/src/pages/login/loginPage.jsx
@@ -4,7 +4,31 @@ const LoginPage = () => {
   return (
     <div className="login-page">
       <div className="login-card">
-        <form></form>
+        <form className="login-form">
+          <h2>Login</h2>
+          <label>
+            First Name:
+            <input type="text" id="first-name" name="first-name"></input>
+          </label>
+          <label>
+            Last Name:
+            <input type="text" id="last-name" name="last-name"></input>
+          </label>
+          <label>
+            Email:
+            <input type="email" id="email" name="email" required></input>
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+            ></input>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
