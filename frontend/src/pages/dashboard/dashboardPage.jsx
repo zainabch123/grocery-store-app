d44550ae16eb1de639b54cc84ca8ea68856aae2f
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-08-22 16:12:11 +0100

    added image carousel

--- frontend/src/pages/dashboard/dashboardPage.jsx
+++ frontend/src/pages/dashboard/dashboardPage.jsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom";
 import heroImage1 from "../../assets/heroImage1.jpg";
+import heroImage2 from "../../assets/heroImage2.jpg";
+import heroImage3 from "../../assets/heroImage3.jpg";
 import Header from "../../components/header";
 import "./dashboardPage.css";
+import { useState } from "react";
 
 const DashboardPage = () => {
+  const heroImg = [heroImage1, heroImage2, heroImage3];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  console.log(currentIndex);
+
+  function nextHeroImg() {
+    setCurrentIndex((i) => (i === heroImg.length - 1 ? 0 : i + 1));
+  }
+
+  function prevHeroImg() {
+    setCurrentIndex((i) => (i === 0 ? heroImg.length - 1 : i - 1));
+  }
+
   return (
     <div className="dashboard-page">
       <Header />
@@ -17,7 +32,15 @@ const DashboardPage = () => {
             <Link to="">Electronics</Link>
           </div>
         </nav>
-        <div className="hero-image-carousel"><img src={heroImage1}/></div>
+        <div className="hero-image-carousel">
+          <button className="prev-button" onClick={prevHeroImg}>
+            {"<"}
+          </button>
+          <img src={heroImg[currentIndex]} />
+          <button className="next-button" onClick={nextHeroImg}>
+            {">"}
+          </button>
+        </div>
         <div style={{ height: "100px" }}>Today's Deals</div>
         <div>Something else</div>
       </div>
